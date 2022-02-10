29a7e10777ebf1e6bd536f0526899e25510af7be
Liliana Repo <lili98ps@gmail.com>
2022-02-09 18:05:52 -0600

    Java file with operations: addition, substraction, division, multiplication and square root

--- Javafile.java
+++ Javafile.java
@@ -1,10 +1,32 @@
+import java.lang.Math;
+
 public class Javafile{
 	public int add(int a, int b){
 		return a + b;
 	}
 
+	public int sub(int a, int b){
+		return a - b;
+	}
+
+	public int div(int a, int b){
+		return a / b;
+	}
+
+	public int mult(int a, int b){
+		return a * b;
+	}
+
+	public double sqrtMethod(double a){
+		return Math.sqrt(a);
+	}
+
 	public static void main(String[] args) {
 		Javafile jf = new Javafile();
 		jf.add(5,5);
+		jf.sub(4,3);
+		jf.div(6,2);
+		jf.mult(3,2);
+		jf.sqrtMethod(25.0);
 	}
 }
