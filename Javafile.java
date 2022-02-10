8e35071647529a0d6ba39f4a76cd48b96226db66
Liliana Repo <lili98ps@gmail.com>
2022-02-09 19:47:10 -0600

    New line from grandparent to tail commit

--- Javafile.java
+++ Javafile.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 
 public class Javafile{
 	public int add(int a, int b){
+		System.out.println("This is add method");
 		System.out.println("This is add method for 2 params");
 		return a + b;
 	}
